102a30f965aa022843922309b93d29b158de5710
Daniel White <dawhite@seek.com.au>
2017-02-16 03:37:21 +1100

    Introduce a plugin for cloning events

--- plugin/clone/clone.go
+++ plugin/clone/clone.go
@@ -2,13 +2,24 @@ package main
 
 import "github.com/danielwhite/dumbledore/plugin"
 
-type cloneFilter struct {
+func main() {}
+
+// New creates a new filter configured with the provided options.
+func New(opts plugin.Options) (plugin.Filter, error) {
+	var f filter
+	if err := opts.Decode(&f); err != nil {
+		return nil, err
+	}
+	return &f, nil
+}
+
+type filter struct {
 	// A new clone will be created with the given type for each
 	// type in this list.
 	Clones []string
 }
 
-func (f *cloneFilter) Filter(event plugin.Event, out chan<- plugin.Event) {
+func (f *filter) Filter(event plugin.Event, out chan<- plugin.Event) {
 	for _, name := range f.Clones {
 		// KLUDGE: Shallow copy; real thing should do a deep copy.
 		clone := plugin.Event{}
